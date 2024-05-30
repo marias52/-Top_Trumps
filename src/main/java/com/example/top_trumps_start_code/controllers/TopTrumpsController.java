001c39209f322b7e26f7f408d0d41b9a8107356b
Faith Peddie <faithpeddie@outlook.com>
2024-05-30 16:50:03 +0100

    finished mvp

--- src/main/java/com/example/top_trumps_start_code/controllers/TopTrumpsController.java
+++ src/main/java/com/example/top_trumps_start_code/controllers/TopTrumpsController.java
@@ -1,32 +1,27 @@
 package com.example.top_trumps_start_code.controllers;
 
 import com.example.top_trumps_start_code.models.Card;
+import com.example.top_trumps_start_code.services.TopTrumpsServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 
 @RestController
 @RequestMapping (value = "/toptrumps")
+
 public class TopTrumpsController {
 
-
+@Autowired
+TopTrumpsServices topTrumpsServices;
 
     @PostMapping
-    public ResponseEntity<ArrayList<Card>> game(@RequestBody ArrayList<Card> cards) {
-//   we want to take 2 card objects from request body
-
-        ArrayList<Card> result = new ArrayList<>();
-        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    public ResponseEntity<String> game(@RequestBody ArrayList<Card> cards) {
+        String winner = topTrumpsServices.checkWinner(cards);
+        return new ResponseEntity<>(winner, HttpStatus.OK);
     }
 
-
-//    .processGuess(card);
-
-//    if card1.rank > card2.rank then return card1
 }
--- src/main/java/com/example/top_trumps_start_code/services/TopTrumpsServices.java
+++ src/main/java/com/example/top_trumps_start_code/services/TopTrumpsServices.java
@@ -1,7 +1,38 @@
 package com.example.top_trumps_start_code.services;
 
+import com.example.top_trumps_start_code.models.Card;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.CSS;
+import java.util.ArrayList;
+
+@Service
 public class TopTrumpsServices {
 
-//      Card card1 = cards.get(0);
-//       Card card2 = cards.get(1);
+    private ArrayList<Card> cards;
+
+    public TopTrumpsServices() {
+
+    }
+
+    public String checkWinner(ArrayList<Card> cards) {
+        Card card1 = cards.get(0);
+        Card card2 = cards.get(1);
+        if (card1.getCardValue() > card2.getCardValue()) {
+            return String.format("%s of %s wins!", card1.getRank(), card1.getSuit());
+        } else if (card1.getCardValue() < card2.getCardValue()) {
+            return String.format("%s of %s wins!", card2.getRank(), card2.getSuit());
+        }
+        else {
+            return "It's a tie";
+        }
+    }
+
+
+    // Create a TopTrumpsService class that has a method called checkWinner().
+    // This method should take in the 2 card objects.
+
+    //After determining which is the higher value card,
+    // send back a response with the appropriate message (e.g. "Queen of Hearts wins!")
 }
